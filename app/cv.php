d29571fac4febb215abd301d21acba339d49ea3e
Alex QAsper <semidog@gmail.com>
2022-11-09 17:17:52 +0500

    add working expirence

--- app/cv.php
+++ app/cv.php
@@ -14,7 +14,16 @@ $skills = [
   'media' => '50%',
   'copyrighting' => '100%'
 ];
+
+$work_exp = [
+    'Graphic Designer' => 'Jun 2010 - Mar 2012',
+    'Web Developer' => 'Mar 2012 - Dec 2014',
+    'Front End Developer' => 'Jan 2015 - Current'
+];
+
 ?>
+
+
 
 <!DOCTYPE html>
 <html>
@@ -113,7 +122,11 @@ $skills = [
         <div class="w3-container w3-card w3-white w3-margin-bottom">
           <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
           <div class="w3-container">
-            <h5 class="w3-opacity"><b>Front End Developer </b></h5>
+            <h5 class="w3-opacity"><b>
+
+            <?php echo ucfirst($workName); ?>
+
+            </b></h5>
             <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
             <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
             <hr>
--- app/index.php
+++ app/index.php
@@ -1,16 +1,53 @@
+<?php
+$work = [
+    'Graphic Designer' => 'Jun 2010 - Mar 2012',
+    'Web Developer' => 'Mar 2012 - Dec 2014',
+    'Front End Developer' => 'Jan 2015 - Current'
+];
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title> Опыт работы </title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="styles/style.css">
 </head>
-<body>
-    <h1>Главная</h1>
-    <?php
-        echo 'Игорь';
-        print('igor');
-    ?>
+
+
+<body class="w3-light-grey">
+
+  <div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+    <div class="w3-row-padding">
+    <h1>Опыт работы</h1>
+
+<?php foreach ($work as $workName => $workValue) : ?>
+    <h5 class="w3-opacity">
+        <p>
+            <?php
+                echo ucfirst($workName);
+            ?>
+        </p>
+    </h5>
+        <div class="w3-container">
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+                <?php
+                    echo $workValue;
+                ?>
+<hr>
+            </div>
+        </div>
+
+<?php endforeach; ?>
+
 </body>
 </html>
+
